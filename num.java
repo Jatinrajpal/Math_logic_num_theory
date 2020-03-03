b44e316866a6a965eae9f5a32421f3cdb8b8a42d
JATIN RAJPAL <jatinrajpal57@gmail.com>
2020-03-04 00:05:21 +0530

    Create num.java
    This is code which is used to check if the number is a series of 2^n,and prints "YES" if it follows.

---
+++ num.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class num
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+int n;
+n=sc.nextInt();
+if((n&(n-1))==0)
+{
+System.out.println("YES");
+}
+else
+{
+System.out.println("NO");
+
+}
+}
+}
